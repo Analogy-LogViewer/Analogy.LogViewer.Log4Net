ab96016d9d202eadbabf1bbc3bacde0ee8c6fdbd
Lior Banai <36262995+LiorBanai@users.noreply.github.com>
2020-04-22 18:00:24 +0300

    add user control

---
+++ Analogy.LogViewer.Log4Net/UserSetttingsUC.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Analogy.LogViewer.Log4Net
+{
+    public partial class UserSetttingsUC : UserControl
+    {
+        public UserSetttingsUC()
+        {
+            InitializeComponent();
+        }
+    }
+}
