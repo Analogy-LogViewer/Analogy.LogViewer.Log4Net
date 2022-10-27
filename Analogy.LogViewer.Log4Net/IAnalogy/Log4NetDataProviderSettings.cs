a2f6ea215f717a12d0398dc65928711f5debc67d
Lior Banai <liorbanai@gmail.com>
2022-10-27 11:04:23 +0300

    update nugets

--- Analogy.LogViewer.Log4Net/IAnalogy/Log4NetDataProviderSettings.cs
+++ Analogy.LogViewer.Log4Net/IAnalogy/Log4NetDataProviderSettings.cs
@@ -8,14 +8,19 @@ using Analogy.Interfaces;
 
 namespace Analogy.LogViewer.Log4Net.IAnalogy
 {
-    public class Log4NetDataProviderSettings : Template.UserSettingsFactory
+    public class Log4NetDataProviderSettings : Template.TemplateUserSettingsFactory
     {
         public override string Title { get; set; } = "Log4Net settings";
-        public override UserControl DataProviderSettings { get; set; } = new UserSetttingsUC();
+        public override UserControl DataProviderSettings { get; set; }
         public override Image? SmallImage { get; set; } = Resources.log4net16x16;
         public override Image? LargeImage { get; set; } = Resources.log4net32x32;
         public override Guid FactoryId { get; set; } = Log4NetFactory.Log4NetFactoryId;
         public override Guid Id { get; set; } = new Guid("2D09F7E7-C55E-41B0-8068-A474D2361F85");
+
+        public override void CreateUserControl(IAnalogyLogger logger)
+        {
+            DataProviderSettings = new UserSetttingsUC();
+        }
 
         public override Task SaveSettingsAsync()
         {
